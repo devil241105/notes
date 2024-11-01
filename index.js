bdf3878cd89030f57e903d9f93514be6583e7c52
Ankit <AnkitKumar241105@gmail.com>
2024-11-01 16:31:25 +0530

    changed db and added signup and login routes

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import pg from "pg";
 import methodOverride from "method-override";
+import bcrypt from "bcrypt";
 
 const app = express();
-const port = process.env.PORT;
+const port = 3000;
 
 const db = new pg.Client({
     user: "postgres",
@@ -14,12 +15,77 @@ const db = new pg.Client({
 db.connect();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 
 
+app.get("/signup", async (req, res) => {
+    try {
+        res.render("signup");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
+    }
+});
 
+app.post("/signup", async (req, res) => {
+    const { name, password } = req.body;
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const query = "INSERT INTO users (name, password) VALUES ($1, $2) RETURNING *";
+        const values = [name, hashedPassword];
+
+        const result = await db.query(query, values);
+        res.status(201).send("User registered successfully!");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+
+
+app.get("/login", async (req, res) => {
+    try {
+        res.render("login");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.post("/login", async (req, res) => {
+    const { name, password } = req.body;
+
+    try {
+        const query = "SELECT * FROM users WHERE name = $1";
+        const result = await db.query(query, [name]);
+
+        if (result.rows.length === 0) {
+            return res.status(400).send("User not found");
+        }
+        const user = result.rows[0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send("Incorrect password");
+        }
+
+        res.send("Login successful!");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+
+
+//CRUD
 app.get("/notes", async (req, res) => {
     try {
         const result = await db.query("SELECT * FROM notes ORDER BY created_at DESC");
