91e826cb1a0c00afd715e75fbb7860b1ee881534
Ankit <AnkitKumar241105@gmail.com>
2024-11-01 22:59:40 +0530

    added individualism and jwt

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import pg from "pg";
 import methodOverride from "method-override";
 import bcrypt from "bcrypt";
+import { jwtAuthMiddleware, generateToken } from './jwt.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -26,25 +29,43 @@ app.get("/signup", async (req, res) => {
         res.render("signup");
     } catch (err) {
         console.error(err);
-        res.status(500).send("Server Error");
+        res.status(500).send("Server Error(signup-get)");
     }
 });
 
 app.post("/signup", async (req, res) => {
-    const { name, password } = req.body;
+    const { username, password } = req.body;
+    // console.log("Received data:", req.body);
 
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const query = "INSERT INTO users (name, password) VALUES ($1, $2) RETURNING *";
-        const values = [name, hashedPassword];
-
+        const query = "INSERT INTO users (name, password) VALUES ($1, $2) RETURNING uid";
+        const values = [username, hashedPassword];
         const result = await db.query(query, values);
-        res.status(201).send("User registered successfully!");
+        const uid = result.rows[0].uid;
+        const token = generateToken({ uid });
+        console.log("Token is : ", token);
+        const tableName = `user_${uid}`;
+        const createTableQuery = `
+            CREATE TABLE ${tableName} (
+                id SERIAL PRIMARY KEY,
+                note_title VARCHAR(255),
+                note_content TEXT,
+                created_at TIMESTAMP DEFAULT NOW()
+            );
+        `;
+        await db.query(createTableQuery);
+        res.redirect("./notes");
     } catch (err) {
-        console.error(err);
-        res.status(500).send("Server Error");
+        if (err.code === '23505') {  //unique violation code
+            res.status(400).json({ error: "Username already exists" });
+        } else {
+            console.error(err);
+            res.status(500).send("Server Error(signup-post)");
+        }
     }
 });
+
 
 
 
@@ -54,7 +75,7 @@ app.get("/login", async (req, res) => {
         res.render("login");
     } catch (err) {
         console.error(err);
-        res.status(500).send("Server Error");
+        res.status(500).send("Server Error(login-get)");
     }
 });
 
@@ -74,11 +95,13 @@ app.post("/login", async (req, res) => {
         if (!isMatch) {
             return res.status(400).send("Incorrect password");
         }
+        const token = generateToken({ uid: user.uid });
+        console.log("Token is : ", token);
 
-        res.send("Login successful!");
+        res.redirect("./notes");
     } catch (err) {
         console.error(err);
-        res.status(500).send("Server Error");
+        res.status(500).send("Server Error(login-post)");
     }
 });
 
@@ -86,66 +109,69 @@ app.post("/login", async (req, res) => {
 
 
 //CRUD
-app.get("/notes", async (req, res) => {
+app.get("/notes",jwtAuthMiddleware, async (req, res) => {
+    const tableName = `user_${req.user.uid}`;
     try {
-        const result = await db.query("SELECT * FROM notes ORDER BY created_at DESC");
+        const result = await db.query(`SELECT * FROM ${tableName} ORDER BY created_at DESC`);
         // console.log("Full result:", result);
         // console.log("Rows:", result.rows);
         res.render("notes", { notes: result.rows });
     } catch (err) {
         console.error(err);
-        res.status(500).send("Server Error");
+        res.status(500).send("Server Error get");
     }
 });
 
 
-app.post("/notes", async (req, res) => {
+app.post("/notes",jwtAuthMiddleware, async (req, res) => {
     const { title, content } = req.body;
+    const tableName = `user_${req.user.uid}`;
     // console.log("title: ", title);
     // console.log("content: ", content);
     try {
-        const query = "INSERT INTO notes (title, content, created_at) VALUES ($1, $2, NOW()) RETURNING *";
+        const query = `INSERT INTO ${tableName} (title, content, created_at) VALUES ($1, $2, NOW()) RETURNING *`;
         const values = [title, content];
         await db.query(query, values);
         res.redirect("/notes");
     } catch (err) {
         console.error(err);
-        res.status(500).send("Server Error");
+        res.status(500).send("Server Error post");
     }
 });
 
 
-app.delete("/notes/:id", async (req, res) => {
-    // const { id } = req.params;
+app.delete("/notes/:id",jwtAuthMiddleware, async (req, res) => {
     const id = req.params.id;
     // console.log("id: ", id);
+    const tableName = `user_${req.user.uid}`;
     try {
-        const query = "DELETE FROM notes WHERE id = $1";
+        const query = `DELETE FROM ${tableName} WHERE id = $1`;
         await db.query(query, [id]);
         res.redirect("/notes");
     } catch (err) {
         console.error(err);
-        res.status(500).send("Server Error");
+        res.status(500).send("Server Error delete");
     }
 });
 
-app.put("/notes/:id", async (req, res) => {
+app.put("/notes/:id",jwtAuthMiddleware, async (req, res) => {
     const id = req.params.id;
     // const { title, content } = req.body;
     const title = req.body.title;
     const content = req.body.content;
+    const tableName = `user_${req.user.uid}`;
 
     console.log("Updating note with ID:", id);
 
     try {
-        const query = "UPDATE notes SET title = $1, content = $2 WHERE id = $3 RETURNING *";
+        const query = `UPDATE ${tableName} SET title = $1, content = $2 WHERE id = $3 RETURNING *`;
         const values = [title, content, id];
         await db.query(query, values);
         res.redirect("/notes");
 
     } catch (err) {
         console.error(err);
-        res.status(500).send("Server Error");
+        res.status(500).send("Server Error put");
     }
 });
 
--- jwt.js
+++ jwt.js
@@ -1,33 +1,22 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 
-const jwtAuthMiddleware = (req, res, next) => {
+export const jwtAuthMiddleware = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) return res.status(401).json({ error: 'Token Not Found' });
 
-    // first check request headers has authorization or not
-    const authorization = req.headers.authorization
-    if(!authorization) return res.status(401).json({ error: 'Token Not Found' });
+    const token = authorization.split(' ')[1];
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
 
-    // Extract the jwt token from the request headers
-    const token = req.headers.authorization.split(' ')[1];
-    if(!token) return res.status(401).json({ error: 'Unauthorized' });
-
-    try{
-        // Verify the JWT token
+    try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        // Attach user information to the request object
-        req.user = decoded
+        req.user = decoded;
         next();
-    }catch(err){
+    } catch (err) {
         console.error(err);
         res.status(401).json({ error: 'Invalid token' });
     }
-}
+};
 
-
-// Function to generate JWT token
-const generateToken = (userData) => {
-    // Generate a new JWT token using user data
-    return jwt.sign(userData, process.env.JWT_SECRET, {expiresIn: 30000});
-}
-
-module.exports = {jwtAuthMiddleware, generateToken};
+export const generateToken = (userData) => {
+    return jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: 30000 });
+};
